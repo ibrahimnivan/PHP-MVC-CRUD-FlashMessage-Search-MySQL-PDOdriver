614de64040985481e11f9712e6c265759af678e7
ibrahimnivan <ibrahimnivan@gmail>
2024-01-07 20:12:49 +0700

    model from mysql

--- MVC.md
+++ MVC.md
@@ -56,3 +56,32 @@ __RewriteRule ^(.*)$ index.php?url=$1 [L]__ If the above conditions are met, thi
 11. menggunakan data dari __model__ di home index
 `<p class="lead">Halo nama saya <?= $data['nama']; ?></p>`
 
+12. terhubung dan query ke mysql menggunakan __PDO__
+```php
+class Mahasiswa_model {
+  private $dbhandler;
+  private $statement;
+
+  public function __construct() {
+    // data source name
+    $dsn = 'mysql:host=localhost;dbname=phpmvc'; 
+
+    try {
+      $this->dbhandler = new PDO($dsn, 'root', '');
+    } catch(PDOException $e) {
+      die($e->getMessage()); // menghentikan program
+    }
+
+  }
+
+    public function getAllMhs() {
+      $this->statement = $this->dbhandler->prepare('SELECT * FROM mahasiswa');
+      $this->statement->execute();
+      return $this->statement->fetchAll(PDO::FETCH_ASSOC); //dikembalikan sbg array assoc
+    }
+}
+```
+
+13. database wrapper
+- mengelola database untuk berbagai model manapun
+
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,39 +1,23 @@
 <?php
 class Mahasiswa_model {
-  private $mhs = [
-    [
-      "nama" => "Homelander",
-      "nrp" => "1111111",
-      "email" => "homelander@gmail.com",
-      "jurusan" => "teknik sipil"
-    ],
-    [
-      "nama" => "Queen Meave",
-      "nrp" => "2222222",
-      "email" => "queenmeave@gmail.com",
-      "jurusan" => "teknik elektro"
-    ],
-    [
-      "nama" => "A-Train",
-      "nrp" => "3333333",
-      "email" => "atrain@gmail.com",
-      "jurusan" => "teknik kelautan"
-    ],
-    [
-      "nama" => "Black Noir",
-      "nrp" => "4444444",
-      "email" => "blacknoir@gmail.com",
-      "jurusan" => "kehutanan"
-    ],
-    [
-      "nama" => "Willian Butcher",
-      "nrp" => "5555555",
-      "email" => "wilianbutcher@gmail.com",
-      "jurusan" => "managemen"
-    ],
-    ];
+  private $dbhandler;
+  private $statement;
+
+  public function __construct() {
+    // data source name
+    $dsn = 'mysql:host=localhost;dbname=phpmvc'; 
+
+    try {
+      $this->dbhandler = new PDO($dsn, 'root', '');
+    } catch(PDOException $e) {
+      die($e->getMessage()); // menghentikan program
+    }
+
+  }
 
     public function getAllMhs() {
-      return $this->mhs;
+      $this->statement = $this->dbhandler->prepare('SELECT * FROM mahasiswa');
+      $this->statement->execute();
+      return $this->statement->fetchAll(PDO::FETCH_ASSOC); //dikembalikan sbg array assoc
     }
 }
